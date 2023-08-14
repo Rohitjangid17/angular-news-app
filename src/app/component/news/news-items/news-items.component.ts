23a4233fbeeb4ea3dcc2f349e03053954b2c5822
monu_jangid76 <jangidrohit817@gmail.com>
2023-08-14 18:27:36 +0530

    remove news items component

--- src/app/component/news/news-items/news-items.component.ts
+++ src/app/component/news/news-items/news-items.component.ts
@@ -1,28 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Article, ArticlesResponse } from 'src/app/model/news';
-import { CommonService } from 'src/app/service/common.service';
 
 @Component({
   selector: 'app-news-items',
   templateUrl: './news-items.component.html',
   styleUrls: ['./news-items.component.scss']
 })
-export class NewsItemsComponent implements OnInit {
-  newsItems: Article[] = []
+export class NewsItemsComponent {
 
-  constructor(
-    private _commonService: CommonService
-  ) { }
-
-  ngOnInit(): void {
-    this.getNews();
-  }
-
-
-  getNews() {
-    this._commonService.getNewsData().subscribe((res: ArticlesResponse) => {
-      this.newsItems = res.articles;
-      console.log(this.newsItems)
-    })
-  }
 }
