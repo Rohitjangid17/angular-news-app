9e9467d48feb138fd9b7b3e7a4badf1f72d4c273
monu_jangid76 <jangidrohit817@gmail.com>
2023-08-14 16:26:18 +0530

    add some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { NewsComponent } from './component/news/news.component';
+import { NewsItemsComponent } from './component/news/news-items/news-items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    NewsComponent,
+    NewsItemsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/news/news-items/news-items.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-news-items',
+  templateUrl: './news-items.component.html',
+  styleUrls: ['./news-items.component.scss']
+})
+export class NewsItemsComponent {
+
+}
